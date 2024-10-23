9f9dbfbc6437db7619b6bf05f367888b43da9b93
Dilhara-Disanayaka <dilhara.22@cse.mrt.ac.lk>
2024-10-24 00:51:13 +0530

    Add models for employee management

--- src/modules/pim/models/employeeModel.js
+++ src/modules/pim/models/employeeModel.js
@@ -6,8 +6,29 @@ const getAllEmployees = async () => {
     const [rows] = await db.query(query);
     return rows[0];
 }
+const saveEmployeeinfo=async(Personalinfo )=>{
+    const query=`CALL UPDATE_EMPLOYEE_BY_ID(?,?,?,?,?,?,?,?,?,?)`
+    console.log(query);
+    const [rows]=await db.query(query,[Personalinfo.employee_id,Personalinfo.name,Personalinfo.email,Personalinfo.address,Personalinfo.birthday,Personalinfo.martial_status,Personalinfo.job_title,Personalinfo.pay_grade,Personalinfo.employment_status,Personalinfo.branch]);
+    return rows[0];
+}
+const addEmployeeinfo=async(Personalinfo)=>{
+    const query=`CALL ADD_EMPLOYEE(?,?,?,?,?,?,?,?,?,?)`;
+    console.log(query);
+    const[rows]=await db.query(query,[Personalinfo.name,Personalinfo.email,Personalinfo.address,Personalinfo.birthday,Personalinfo.martial_status,Personalinfo.job_title,Personalinfo.pay_grade,Personalinfo.employment_status,Personalinfo.branch,Personalinfo.supervisor]);
+    return rows[0];
+}
+const getEmployeeinfo=async(employee_id)=>{
+    const query = `CALL GET_ALL_EMPLOYEES(?)`;
+    console.log(query,[employee_id]);
+    const [rows] = await db.query(query,[employee_id]);
+    return rows;
+}
 
 
 module.exports = {
-    getAllEmployees
+    getAllEmployees,
+    saveEmployeeinfo,
+    addEmployeeinfo,
+    getEmployeeinfo
 };
