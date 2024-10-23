dc255c609b8a7b1e62385f0906138c519f8b8387
Dilhara-Disanayaka <dilhara.22@cse.mrt.ac.lk>
2024-10-24 01:28:23 +0530

    Add controllers for handling employee requests

--- src/modules/pim/controllers/employeeController.js
+++ src/modules/pim/controllers/employeeController.js
@@ -10,7 +10,46 @@ const getAllEmployees = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 }
+const updateEmployeeinfo=async(req,res)=>{
+    console.log("save personal information");
+    const Personal_info=req.body;
+    const {employee_id}=req.params;
+    try{
+        const updatedemployee=await Employee.saveEmployeeinfo({...Personal_info,employee_id:employee_id});
+        return res.status(200).json({ message: "Employee updated successfully",updatedemployee });
+    }catch(error){
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
+}
+const addEmployeeinfo=async(req,res)=>{
+    console.log("addEmployeeinfo");
+    const Personalinfo=req.body;
+    const supervisor=req.user.employee_id
+    try{
+        const newemployee=await Employee.addEmployeeinfo({ ...Personalinfo, supervisor: supervisor });
+        return res.status(201).json({ message: "Employee added successfully",newemployee });
+    }catch(error){
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
+}
+const getEmployeeinfo=async(req,res)=>{
+    console.log("getEmployeeinfo");
+    const {employee_id}=req.params;
+    try {
+        const employeeinfo = await Employee.getEmployeeinfo(employee_id);
+        return res.status(200).json(employeeinfo);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: error.message });
+    }
+
+}
 
 module.exports = {
-    getAllEmployees
+    getAllEmployees,
+    updateEmployeeinfo,
+    addEmployeeinfo,
+    getEmployeeinfo
 };
