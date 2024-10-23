0c2c7752eef1d42041e06679d2b17292e49fdeb3
Dilhara-Disanayaka <dilhara.22@cse.mrt.ac.lk>
2024-10-24 01:34:30 +0530

    Add APIS to index file

--- src/modules/pim/index.js
+++ src/modules/pim/index.js
@@ -4,5 +4,8 @@ const employeeController = require('./controllers/employeeController');
 const route = express.Router();
 
 route.get('/employees', employeeController.getAllEmployees);
+route.get('/employees/update/:employee_id', employeeController.updateEmployeeinfo);
+route.get('/employees/add', employeeController.addEmployeeinfo);
+route.get('/employees/getdetails/:employee_id', employeeController.getEmployeeinfo);
 
 module.exports = route;
